ef2b93be0a42ebb3c2f4da87b16ab24c229476d8
Honghao Li <hli@greenactionstudio.com>
2023-06-30 14:11:44 -0400

    update account id

--- lib/ci-cd-aws-pipeline-demo-stack.ts
+++ lib/ci-cd-aws-pipeline-demo-stack.ts
@@ -1,35 +1,60 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import { CodePipeline, CodePipelineSource, ShellStep, Step } from 'aws-cdk-lib/pipelines';
-import { ManualApprovalStep } from 'aws-cdk-lib/pipelines';
-import { MyPipelineAppStage } from './stage';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import {
+  CodePipeline,
+  CodePipelineSource,
+  ShellStep,
+  CodeBuildStep,
+} from "aws-cdk-lib/pipelines";
+import { ManualApprovalStep } from "aws-cdk-lib/pipelines";
+import { MyPipelineAppStage } from "./stage";
+import { PolicyStatement } from "aws-cdk-lib/aws-iam";
 
 export class CiCdAwsPipelineDemoStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const pipeline = new CodePipeline(this, 'Pipeline', {
-      pipelineName: 'TestPipeline',
-      synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub('beabetterdevv/ci-cd-aws-pipeline-demo', 'main'), //Remember to change 
-        commands: ['npm ci', 
-                   'npm run build', 
-                   'npx cdk synth']
-      })
+    const pipeline = new CodePipeline(this, "Pipeline", {
+      pipelineName: "TestPipeline",
+      synth: new CodeBuildStep("Synth", {
+        input: CodePipelineSource.gitHub(
+          "Honghao-Li-98/ci-cd-aws-pipeline-demo",
+          "main"
+        ), //Remember to change
+        commands: ["npm ci", "npm run build", "npx cdk synth"],
+      }),
     });
 
+    const testingStage = pipeline.addStage(
+      new MyPipelineAppStage(this, "test", {
+        env: { account: "106367372430", region: "us-east-1" },
+      })
+    );
 
+    testingStage.addPre(
+      new CodeBuildStep("Run Unit Tests", {
+        commands: ["npm install", "npm test"],
+        rolePolicyStatements: [
+          new PolicyStatement({
+            actions: ["sts:AssumeRole"],
+            resources: ["*"],
+            conditions: {
+              StringEquals: {
+                "iam:ResourceTag/aws-cdk:bootstrap-role": "lookup",
+              },
+            },
+          }),
+        ],
+      })
+    );
+    testingStage.addPost(
+      new ManualApprovalStep("Manual approval before production")
+    );
 
-    const testingStage = pipeline.addStage(new MyPipelineAppStage(this, "test", {
-      env: { account: "755314965794", region: "us-east-1" }
-    }));
-
-
-    testingStage.addPre(new ShellStep("Run Unit Tests", { commands: ['npm install', 'npm test'] }));
-    testingStage.addPost(new ManualApprovalStep('Manual approval before production'));
-
-    const prodStage = pipeline.addStage(new MyPipelineAppStage(this, "prod", {
-      env: { account: "755314965794", region: "us-east-1" }
-    }));
+    const prodStage = pipeline.addStage(
+      new MyPipelineAppStage(this, "prod", {
+        env: { account: "106367372430", region: "us-east-1" },
+      })
+    );
   }
 }
